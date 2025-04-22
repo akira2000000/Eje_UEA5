1db71f7e38a051666b53fce371f802beef93df36
akira2000000 <angyg4lvez@gmail.com>
2025-04-21 22:38:39 -0600

    cambios

--- CuartoB1/script.js
+++ CuartoB1/script.js
@@ -1,18 +1,11 @@
 const introScreen = document.querySelector(".intro-screen");
 const scrollContainer = document.querySelector(".scrollable");
-const enlaceInicio = document.getElementById("enlace-inicio");
 const enlaceFinal = document.getElementById("enlace-final");
 
 // LINK SIG PAG
     // Mostrar/ocultar enlaces según posición del scroll
     scrollContainer.addEventListener("scroll", () => {
         const scrollMax = scrollContainer.scrollWidth - scrollContainer.clientWidth;
-  
-        if (scrollContainer.scrollLeft > 20) {
-          enlaceInicio.style.display = "none";
-        } else {
-          enlaceInicio.style.display = "block";
-        }
   
         if (scrollContainer.scrollLeft >= scrollMax - 5) {
           enlaceFinal.style.display = "block";
